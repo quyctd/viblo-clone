a17ae3ab42686abd79f2fd5b3bcc3eeb32e33b86
Quý Đinh <quy.dc98@gmail.com>
2019-06-22 15:58:52 +0700

    Update requirements

--- fg-blog-API/VibloAsia/settings/production.py
+++ fg-blog-API/VibloAsia/settings/production.py
@@ -5,29 +5,100 @@ from .development import *
 DEBUG = False
 
 # You will have to determine, which hostnames should be served by Django
-ALLOWED_HOSTS = []
 
 # ##### SECURITY CONFIGURATION ############################
 
-# TODO: Make sure, that sensitive information uses https
-# TODO: Evaluate the following settings, before uncommenting them
-# redirects all requests to https
-# SECURE_SSL_REDIRECT = True
-# session cookies will only be set, if https is used
-# SESSION_COOKIE_SECURE = True
-# how long is a session cookie valid?
-# SESSION_COOKIE_AGE = 1209600
+# allow all hosts during development
+ALLOWED_HOSTS = ['*']
 
-# validates passwords (very low security, but hey...)
-# AUTH_PASSWORD_VALIDATORS = [
-#    { 'NAME': 'django.contrib.authen.password_validation.UserAttributeSimilarityValidator', },
-#    { 'NAME': 'django.contrib.authen.password_validation.MinimumLengthValidator', },
-#    { 'NAME': 'django.contrib.authen.password_validation.CommonPasswordValidator', },
-#    { 'NAME': 'django.contrib.authen.password_validation.NumericPasswordValidator', },
-# ]
 
-# the email address, these error notifications to admins come from
-# SERVER_EMAIL = 'root@localhost'
+# adjust the minimal login
+LOGIN_URL = 'core_login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'core_login'
 
-# how many days a password reset should work. I'd say even one day is too long
-# PASSWORD_RESET_TIMEOUT_DAYS = 1
+
+# ##### DATABASE CONFIGURATION ############################
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': join(PROJECT_ROOT, 'run', 'dev.sqlite3'),
+    }
+}
+
+# ##### APPLICATION CONFIGURATION #########################
+
+INSTALLED_APPS = DEFAULT_APPS + [
+    'django.contrib.sites',
+
+    # 3rd party app
+
+    # basic authen
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+    'taggit',
+    'taggit_serializer',
+
+    # Social authentic
+    'social_django',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
+
+    'apps.api',
+    'apps.authen',
+    'apps.base',
+    'apps.post',
+    'apps.discussion',
+    'corsheaders',
+]
+
+# Time zone
+TIME_ZONE = "Asia/Ho_Chi_Minh"
+
+SITE_ID = 2
+
+
+AUTH_USER_MODEL = 'authen.CustomUser'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MIDDLEWARE += ['corsheaders.middleware.CorsMiddleware']
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+    'social_core.backends.google.GoogleOpenId',  # for Google authentication
+    'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+    'social_core.backends.github.GithubOAuth2',  # for Github authentication
+    'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
+
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+#Taggit
+TAGGIT_CASE_INSENSITIVE = True
+
+#Google login
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='671755523680-kl8oatubi2m7andl8pstad21t9eneoqf.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'M1hDXZbe-M7LVRnJe25dWPXe' #Paste Secret Key
+
+#Facebook login
+SOCIAL_AUTH_FACEBOOK_KEY = '836707276705341'
+SOCIAL_AUTH_FACEBOOK_SECRET = '164a63bb6bcc619f0ee810edec3f8d1e'
